34039d435ebb4d7db8b7184d2ec74a28e7faf276
Bautista Battaglia <bautistabattaglia@gmail.com>
2025-03-05 02:49:37 -0300

    Modern Design test 1

---
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using ModernDesign.Core;
+using System;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; }
+        public RelayCommand NewsViewCommand { get; }
+
+        public readonly HomeViewModel HomeVm;
+        public readonly NewsViewModel NewsVm;
+
+        private object? _currentView;
+
+        public object? CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value ?? HomeVm;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            NewsVm = new NewsViewModel();
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o => CurrentView = HomeVm);
+            NewsViewCommand = new RelayCommand(o => CurrentView = NewsVm);
+        }
+    }
+}
